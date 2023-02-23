30d6e4e72ff32c424697076f57cb38f10ea17575
ralph-hatoum <hatoumralph@gmail.com>
2023-02-23 08:43:59 +0100

    Added comments

--- node_prg.go
+++ node_prg.go
@@ -12,6 +12,7 @@ var wg sync.WaitGroup
 var peers []string
 
 func main() {
+	// Main program
 	defer wg.Done()
 	fmt.Printf("Starting peer program\n")
 	wg.Add(1)
@@ -21,6 +22,7 @@ func main() {
 }
 
 func acceptConnections() {
+	// Node is constantly listening to its peers - this function sets up a tcp server and accepts connections
 	fmt.Println("Attempting to start listening ... ")
 	l, err := net.Listen("tcp", "localhost:60000")
 	if err != nil {
@@ -45,6 +47,12 @@ func acceptConnections() {
 }
 
 func handleConnection(conn net.Conn) {
+	// This function handles the connection - there are several reasons as to why a node would want to contact its peers, so here we distinguish between those
+	// A node can either :
+	// - Start a new connection or join the network
+	// - Ask for storage space
+	// - Ask for the status of its data on a given node
+	// - Ask for data to be deleted
 	defer conn.Close()
 	fmt.Println("New connection detected")
 
@@ -54,6 +62,7 @@ func handleConnection(conn net.Conn) {
 		// New peer joining the network
 		handleNewPeerConn(conn)
 	} else {
+		// Unrecognized message
 		fmt.Println("Message unrecognized - ignoring msg")
 		fmt.Println(peers)
 	}
@@ -61,7 +70,8 @@ func handleConnection(conn net.Conn) {
 }
 
 func handleNewPeerConn(conn net.Conn) {
-
+	// Executed when a new peer connects to us - we send him a conformation with "connect-ok"
+	// and if the peer was not already in our known peers, we add him to the known peers
 	fmt.Println("Peer attempting connection ...")
 	bufio.NewWriter(conn).WriteString("connect-ok\n")
 	remote_addr := conn.RemoteAddr().String()[:strings.IndexByte(conn.RemoteAddr().String(), ':')]
@@ -74,6 +84,8 @@ func handleNewPeerConn(conn net.Conn) {
 }
 
 func addNewPeer(add string) {
+	// This function a node to initiate a connection to another peer
+	// most likely will only be used once when the node is turned on, and only again if the node goes down and wants to come back to the network
 	fmt.Println("Adding new peer", add)
 
 	conn1, err := net.Dial("tcp", add)
@@ -89,14 +101,20 @@ func addNewPeer(add string) {
 	message, _ := bufio.NewReader(conn1).ReadString('\n')
 
 	if message == "connect-ok\n" {
-		fmt.Println("Peer accepted connection - adding peer to peers")
-		peers = append(peers, add)
+		fmt.Println("Peer accepted connection")
+		if alreadyPeer(peers, add) {
+			fmt.Println("Peer was already known")
+		} else {
+			peers = append(peers, add)
+			fmt.Println("Added peer to the network")
+		}
 		fmt.Println(peers)
 	}
 
 }
 
 func alreadyPeer(peers []string, peer string) bool {
+	// checks if the peer is already known
 	found := false
 	for _, v := range peers {
 		if v == peer {
