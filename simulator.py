1caa7d1be041e3f870e03e4e36c276817ed78606
ralph-hatoum <hatoumralph@gmail.com>
2023-02-27 17:01:26 +0100

    simulation not done

--- simulator.py
+++ simulator.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 
 
-
+"""
 def activity_on_node_realtime():
     file_number = 0
     files = []
@@ -40,6 +40,7 @@ def activity_on_node_realtime():
                     file_occurences[file] = 1
 
     print(file_occurences)
+"""
 
 def activity_on_node_generator():
     file_number = 0
@@ -55,6 +56,7 @@ def activity_on_node_generator():
             if a == 0:
                 #print("Created new file file_"+str(file_number))
                 files.append("file_"+str(file_number))
+                file_occurences["file_"+str(file_number)]=0
                 file_number +=1
                 f.write(str(t)[:4]+" Created new file file_"+str(file_number)+"\n")
             if a == 1:
@@ -68,14 +70,28 @@ def activity_on_node_generator():
                     id = np.random.randint(0, len(files))
                     file = files[id]
                     f.write(str(t)[:4]+" Opened file_"+str(id)+"\n")
-                    if file in file_occurences.keys():
-                        file_occurences[file] +=1
-                    else:
-                        file_occurences[file] = 1
+                    file_occurences[file] +=1
 
+    with open("occurences.txt","w")as f:
         for file in list(file_occurences.keys()):
             f.write("File "+file+" was consulted "+str(file_occurences[file])+" times \n")
 
+def generate_new_file(filename, time):
+    
+    importance = np.random.normal(0,0.5)
+    
+    if importance < -0.45:
+        importance=1
+    elif importance >= -0.4 and importance<=0.4:
+        importance=2
+    elif importance > 0.4:
+        importance=3
+
+    return (filename, importance, time)
+    
+    
+#def delete_file
 
 
-activity_on_node_generator()
+
+generate_new_file()
