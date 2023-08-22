3fda8979f4bd5a619d9654c8e0c9f1242b919cca
ralph-hatoum <hatoumralph@gmail.com>
2023-08-22 23:24:48 +0200

    solved DB issue now installing kadeploy

--- grid5k/Docker/scripts/entrypoint.sh
+++ grid5k/Docker/scripts/entrypoint.sh
@@ -1,30 +1,14 @@
 #!/bin/bash
 
-config="default-lease-time 28800; \
-max-lease-time 86400; \
-allow booting; \
-allow bootp; \
-not-authoritative; use-host-decl-names on; \
-subnet 192.168.0.0 netmask 255.255.255.0 { \
-option subnet-mask 255.255.255.0; \
-option broadcast-address 192.168.0.255; option routers 192.168.0.254; \
-option domain-name "testbed.lan"; \
-filename "pxelinux.0"; \
-next-server 192.168.0.1; \
-host node-1.testbed.lan { \
-hardware ethernet 00:09:3d:12:33:e6; \
-fixed-address 192.168.0.10; \
-option host-name "node-1"; \
-} \
-host node-2.testbed.lan { \
-hardware ethernet 00:09:3d:12:33:e7; \
-fixed-address 192.168.0.11; \
-option host-name "node-2"; \
-} }"
+echo "Starting DB"
+sudo service mariadb start
 
+echo "Building DB"
 
-echo $config > /usr/local/etc/dhcpd.conf
+mysql -h localhost -u root -pr00t -e "CREATE DATABASE deploy3;" 
 
-cat /usr/local/etc/dhcpd.conf
+mysql -h localhost -u root -pr00t -e "GRANT select, insert, update, delete, create, drop, alter, create temporary tables, lock tables ON deploy3.* TO 'deploy'@'kadeploy.site.grid5000.fr';"
+
+echo "Done!"
 
 sleep infinity
