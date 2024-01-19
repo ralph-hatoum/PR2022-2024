01b4591b66bf6edb753520cca9973dc44ae995e4
ralph-hatoum <hatoumralph@gmail.com>
2024-01-19 22:26:29 +0100

    bartering confif writer - tested - works

--- bartering_parser.py
+++ bartering_parser.py
@@ -1,5 +1,6 @@
 import json
-
+import subprocess
+import yaml
 
 
 def extract_bartering_config(config_path):
@@ -10,4 +11,38 @@ def extract_bartering_config(config_path):
     bartering_conf = config["Bartering_network"]
 
     failure_model = bartering_conf["FailureModel"]
-    
+
+    output = []
+
+    peers = bartering_conf["peers"]
+
+    for key in peers.keys():
+        host_type = f"BarteringNodes{key}"
+        number_of_nodes = peers[key]["Nodes"]
+        output.append([host_type, number_of_nodes, failure_model ,peers[key]["conf"]])
+
+    # print(output)
+    return output
+
+conf_list = extract_bartering_config("./network_config.json")
+
+def check_if_different_and_change(key, value, conf):
+    if conf[key] != value:
+        conf[key] = value
+
+def build_configs(config_list):
+    with open("bartering-protocol/base-config.yaml", "r") as f:
+        conf = yaml.safe_load(f)
+    # print(base_conf["FailureModel"])
+    for node_type in config_list:
+        subprocess.run(["mkdir",f"bartering-protocol/{node_type[0]}"])
+        check_if_different_and_change("FailureModel", node_type[2], conf)
+        for key in node_type[3].keys():
+            check_if_different_and_change(key, node_type[3][key], conf)
+        with open(f"bartering-protocol/{node_type[0]}/conf.yaml", "w") as conf_file:
+            yaml.dump(conf, conf_file, default_flow_style=False)
+
+build_configs(conf_list)
+
+def write_in_playbooks(config_list):
+    return
